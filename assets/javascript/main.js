fe062d6320833cab391d31ea04d55b28eaa6563b
kjojones <43149867+kfroerer@users.noreply.github.com>
2018-11-14 20:07:03 -0600

    basic set up with links

---
+++ assets/javascript/main.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyCBcB7BThjBY9R4qE4E2u63FkkHYc3fDqw",
+    authDomain: "project1-recipes-dc3ca.firebaseapp.com",
+    databaseURL: "https://project1-recipes-dc3ca.firebaseio.com",
+    projectId: "project1-recipes-dc3ca",
+    storageBucket: "project1-recipes-dc3ca.appspot.com",
+    messagingSenderId: "915749649529"
+  };
+  firebase.initializeApp(config);
