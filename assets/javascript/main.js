a4ed325dd3156369b03deafb9f028f097c7d04b7
Jim Girardeau <jwgirardeau@gmail.com>
2018-11-16 19:11:22 -0600

    added input validation on search box

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -5,28 +5,38 @@ var config = {
     projectId: "project1-recipes-dc3ca",
     storageBucket: "project1-recipes-dc3ca.appspot.com",
     messagingSenderId: "915749649529"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
-  var database = firebase.database();
+var database = firebase.database();
 
-  var listItem1 = "";
-  var listItem2 = "";
-  var listItem3 = "";
+var listItem1 = "";
+var listItem2 = "";
+var listItem3 = "";
 
-  database.ref().set({
-      listItem1,
-      listItem2,
-      listItem3
-  });
+database.ref().set({
+    listItem1,
+    listItem2,
+    listItem3
+});
 
-  $("#submitButton").click(function() {
+function validateInputTextNotEmpty(inputText, htmlRef) {
+    var validText = false;
+    // console.log(inputText, " ", inputText.length)
+    if (inputText.length > 0) {
+        htmlRef.removeClass("errorShow").addClass("errorHide");
+        validText = true;
+    } else {
+        htmlRef.removeClass("errorHide").addClass("errorShow");
+    }
+    return validText;
+}
+
+$("#submitButton").click(function() {
     event.preventDefault();
-    alert('submit')
     var recipeName = $("#recipeSearch").val();
-    var errroInForm=false;
-    if (!validText(recipeName, $("#recipeSearch"))) errorInForm = true;
-    if(!errorInForm){
-        // submit here to search api
+    // console.log(recipeName);
+    if (validateInputTextNotEmpty(recipeName, $("#recipeMissing"))) {
+        // call api
     }
 });
