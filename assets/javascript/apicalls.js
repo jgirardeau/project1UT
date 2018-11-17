60feee2c33c087aed3984206a77c68e5b1f3fe31
DeniseG <denise@girardeau.org>
2018-11-17 11:57:27 -0600

    changed API

--- assets/javascript/apicalls.js
+++ assets/javascript/apicalls.js
@@ -1,26 +1,30 @@
 function getRecipe(item) {
-    var queryURL = "https://www.food2fork.com/api/search?key=332d3c22cb134df1dbac8637c8c2f7a4&q=" + item + "&page=2"
+    var queryURL = "http://api.yummly.com/v1/api/recipes?_app_id=26b02ec4&_app_key=1b6cdc7b7ae0b6805b5689898a4a5311&q="+item
+    // "https://www.food2fork.com/api/search?key=332d3c22cb134df1dbac8637c8c2f7a4&q=" + item + "&page=2"
     $.ajax({
         url: queryURL,
         method: "GET",
     })
         .then(function (response) {
             renderRecipeToHtml(response);
+            console.log(response);
         })
 }
 
 function getIngredients(ingredients) {
-    var queryURL = "https://www.food2fork.com/api/get?key=332d3c22cb134df1dbac8637c8c2f7a4&rId=35382"
+    var queryURL = "http://api.yummly.com/v1/api/recipe/"+ingredients+"?_app_id=26b02ec4&_app_key=1b6cdc7b7ae0b6805b5689898a4a5311"
     $.ajax({
         url: queryURL,
         method: "GET",
     })
         .then(function (response) {
             renderIngredientsToHtml(response);
+            console.log(response);
         })
 }
 
-
+// getRecipe("tofu");
+// getIngredients("Sriracha-Tofu-2164179");
 
 
 
