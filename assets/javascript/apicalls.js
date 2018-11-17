0d3efb1fcabcfcd601b3edcc37a8863026bfd9c1
Jim Girardeau <jwgirardeau@gmail.com>
2018-11-17 15:39:59 -0600

    changed api to https

--- assets/javascript/apicalls.js
+++ assets/javascript/apicalls.js
@@ -1,5 +1,5 @@
 function getRecipe(item) {
-    var queryURL = "http://api.yummly.com/v1/api/recipes?_app_id=26b02ec4&_app_key=1b6cdc7b7ae0b6805b5689898a4a5311&q=" + item
+    var queryURL = "https://api.yummly.com/v1/api/recipes?_app_id=26b02ec4&_app_key=1b6cdc7b7ae0b6805b5689898a4a5311&q=" + item
         // "https://www.food2fork.com/api/search?key=332d3c22cb134df1dbac8637c8c2f7a4&q=" + item + "&page=2"
     $.ajax({
             url: queryURL,
@@ -12,7 +12,7 @@ function getRecipe(item) {
 }
 
 function getIngredients(ingredients) {
-    var queryURL = "http://api.yummly.com/v1/api/recipe/" + ingredients + "?_app_id=26b02ec4&_app_key=1b6cdc7b7ae0b6805b5689898a4a5311"
+    var queryURL = "https://api.yummly.com/v1/api/recipe/" + ingredients + "?_app_id=26b02ec4&_app_key=1b6cdc7b7ae0b6805b5689898a4a5311"
     $.ajax({
             url: queryURL,
             method: "GET",
