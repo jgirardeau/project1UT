decccf1a092b6033572e082e40686c733b2fa4c2
Jim Girardeau <jwgirardeau@gmail.com>
2018-11-21 11:02:14 -0600

    added ingredients to list

--- assets/javascript/writetodom.js
+++ assets/javascript/writetodom.js
@@ -5,6 +5,7 @@ function renderRecipeToHtml(response) {
     } else {
         $("#recipeNotFound").removeClass("errorShow").addClass("errorHide");
     }
+    //getIngredients(recipes[0].id);
     recipes.forEach(function(val) {
         getIngredients(val.id)
     });
@@ -38,13 +39,16 @@ function renderIngredientsToHtml(val) {
     info = addToStringWithComma(info, "Cooking time: ", val.cookTime);
     $("#recipeInformation").text(info);
     $("#recipeURL").attr('href', val.attribution.url)
-    $("#image-1").attr('src', (val.images[0].hostedLargeUrl)); 
+    $("#image-1").attr('src', (val.images[0].hostedLargeUrl));
     addItemsToList($("#recipeGroceryList"), val.ingredientLines);
-    $("#addToGrocery").attr('data','food-'+ val.id);
-    $("#recipeGrocerylist").attr('id','food-'+ val.id);
-// clone
+    $("#addToGrocery").attr('data', 'food-' + val.id);
+    var recipeLocation = $("#recipeGroceryList");
+    $("#recipeGroceryList").attr('id', 'food-' + val.id);
+
+    // clone
     var box2 = $('#hidden-box').clone();
     box2.attr('id', 'box-' + val.id);
     box2.removeClass("hidden");
     box2.appendTo('#box-container');
+    recipeLocation.attr('id', "recipeGroceryList");
 }
