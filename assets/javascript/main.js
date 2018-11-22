ef16bc6e9f5358ecd42575ed7091f6e1f805dbb1
kjojones <43149867+kfroerer@users.noreply.github.com>
2018-11-21 21:47:21 -0600

    added button functions to main js file

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,3 +1,4 @@
+$(document).ready(function(){
 var config = {
     apiKey: "AIzaSyCBcB7BThjBY9R4qE4E2u63FkkHYc3fDqw",
     authDomain: "project1-recipes-dc3ca.firebaseapp.com",
@@ -40,3 +41,28 @@ $("#submitButton").click(function() {
         // call api
     }
 });
+
+    
+$(document).on("click", ".level-item", function(){
+    // console.log(favrecipe);
+    var favDiv = $("<div>");
+    var favrecipe = $("#buttons-test");
+    favDiv.append(favrecipe);
+    $("#fav-recipes").append(favDiv);
+    
+   //will need to change "text" to prepend
+});
+function removeRecipe(){
+    var recipeToRemove = $("#buttons-test");
+    recipeToRemove.remove();
+    // $(this).closest(".article").remove();
+}
+
+$(document).on("click", ".delete", function(){
+    event.preventDefault();
+    console.log("clicked");
+    removeRecipe();
+    // $(this).closest(".article").remove();
+    $(this).parent().remove();
+});
+}) 
