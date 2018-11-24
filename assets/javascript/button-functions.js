3767f472ad2572ca5f9248a98b799b4bff53c47d
kjojones <43149867+kfroerer@users.noreply.github.com>
2018-11-23 21:04:13 -0600

    updated html from master on github

--- assets/javascript/button-functions.js
+++ assets/javascript/button-functions.js
@@ -1,24 +1,10 @@
 $(document).ready(function(){
-    // function removeRecipe(){
-    //     var recipeToRemove = $("#recipe-1");//need recipe id number
-    //     recipeToRemove.remove();
-    // }
-    // function addToFavorites(){
-    //     var newDiv = $("<div>");
-    //     var recipeID = $(this).;//will need id num for each recipe
-    //     newDiv.append(recipeID);
-    //     $("#fav-recipes").append(newDiv);
-        
-    // }
-
     $(document).on("click", ".level-item", function(){
         event.preventDefault();
         var newDiv = $("<div>");
         var recipeToAdd = $(this).closest(".newRecipe");
         newDiv.append(recipeToAdd);
-        $("#fav-recipes").append(newDiv);
-        
-        
+        $("#fav-recipes").append(newDiv);          
     });
    
     $(document).on("click", ".delete", function(){
