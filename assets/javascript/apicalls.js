f20fee75fc800a2cbbe2af18409848f3e4b15f57
Jim Girardeau <jwgirardeau@gmail.com>
2018-11-29 00:24:57 -0600

    clean code; no functional changes

--- assets/javascript/apicalls.js
+++ assets/javascript/apicalls.js
@@ -1,14 +1,11 @@
 function getRecipe(item) {
     var queryURL = "https://api.yummly.com/v1/api/recipes?_app_id=26b02ec4&_app_key=1b6cdc7b7ae0b6805b5689898a4a5311&q=" + item
-        // "https://www.food2fork.com/api/search?key=332d3c22cb134df1dbac8637c8c2f7a4&q=" + item + "&page=2"
-    //console.log("ajax call ", item)
     $.ajax({
             url: queryURL,
             method: "GET",
         })
         .then(function(response) {
             renderRecipeToHtml(response);
-            //  console.log(response);
         })
 }
 
@@ -19,10 +16,6 @@ function getIngredients(ingredients) {
             method: "GET",
         })
         .then(function(response) {
-            // console.log("calling api " + ingredients)
             renderIngredientsToHtml(response);
         })
 }
-
-// getRecipe("tofu");
-// getIngredients("Sriracha-Tofu-2164179");
--- assets/javascript/globals.js
+++ assets/javascript/globals.js
@@ -1,7 +1,18 @@
+// all global vars should be here with an explanation of why they are global and not local
+
+
 //firebase database holder; global so it can be shared across different modules accessing firebase
 var database;
 
 //recipe list; global so multiple callbacks can access
 var recipesInGroceryList = [];
-var recipesInFavoriteList = [];
-var recipesInFavoriteListKeys = [];
+//favorites list; global so multiple callbacks can access
+var recipesInFavoriteList = []; // these two arrays can be combined into one array at a later date
+var recipesInFavoriteListKeys = []; // these two arrays can be combined into one array at a later date
+
+// google API variables -- can probably be done better, but running out of time figuring out the google api
+var map;
+var service;
+var infowindow;
+var markers = [];
+var foodsearch;
